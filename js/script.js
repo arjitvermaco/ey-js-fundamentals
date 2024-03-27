7df1be5483ac846ebb0f80ef7464aeac177f7e1b
Arjit <arjitverma.go@gmail.com>
2024-03-27 10:07:11 +0530

    JS Fundamentals

---
+++ js/script.js
@@ -0,0 +1,118 @@
+// // alert("Hello World!");
+
+// console.log(" My Name is Arjit");
+
+// console.log(2+2)
+// console.log(10*2)
+
+// //Values and variables
+// console.log("Arjit")
+// console.log(1991)
+
+
+// let myName = "Arjit Verma"
+
+// console.log(myName)
+
+// let myBirthYear = 1991;
+
+// console.log(myBirthYear+10)
+
+// //Variable Naming conventions
+
+// let arjit_verma = "AV"
+// let $av = "AV"
+
+// let person = "arjit"
+// let PI = 3.14;
+
+// //CamelCase
+// let arjitVerma = "AV"
+
+// let job1 = "Developer"
+// let job2 = "Trainer"
+
+// let myFirstJob = "Developer"
+// let myCurrentJob = "Trainer"
+
+// //Data Types in js
+// let codingIsFun = false;
+
+// console.log(typeof arjitVerma)
+// console.log(typeof PI)
+// console.log(typeof codingIsFun)
+
+// let year;
+
+// console.log(typeof year)
+
+// year = 1991;
+// console.log(typeof year)
+
+// arjitVerma = 10
+// console.log(typeof arjitVerma)
+
+// //3 different ways to create variables
+// // 1.using let 
+// let age = 31
+// age = 53;
+// console.log(age)
+
+
+
+
+// const canDrive =  true;
+// // canDrive = false
+
+// var lastname = "verma"
+// lastname = "singh"
+// var lastname = "sharma"
+// console.log(lastname)
+
+
+//Basic Operators
+//Math Operations
+
+const currentYear = 2024;
+let myBirthYear = 1991;
+
+let myAge = currentYear - myBirthYear;
+
+let yourAge = currentYear - 2000;
+
+console.log(myAge)
+
+console.log(yourAge)
+
+console.log(myAge * 2,myAge/10)
+
+console.log(2**3)
+// 2**3 means 2 to the power of 3 = 2 *2 *2 
+
+let firstName = "arjit"
+let lastName = "verma"
+
+let fullName = firstName + ' ' +lastName
+console.log(fullName)
+
+//Assignment Operator
+
+let x = 10+15
+x += 10; //x = x+10
+x -=10; //x = x-10;
+x *= 10 // x = x *10;
+
+x++ ; //x = x+1;
+x-- ; //x = x -1;
+
+//Comparision Operators
+// > , < , >= , <=
+console.log(100 > 50)
+
+console.log(currentYear - myBirthYear > currentYear - 2000)
+
+
+//Operator Precesence 
+// BODMAS 
+let z = (10*2)+30 - 10;
+console.log(z)
